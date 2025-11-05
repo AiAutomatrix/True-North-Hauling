5147aa935e02caaaef04d1c066e879be9dcea038
Garret Friesen <cogmoralabs@gmail.com>
2025-11-05 17:43:59 +0000

    right now the auto scroll just blinks through the images but i would lik

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -17,7 +17,7 @@ export default function Gallery() {
   const galleryImages = PlaceHolderImages.filter(p => p.id.startsWith("gallery-"));
 
   const plugin = React.useRef(
-    Autoplay({ delay: 3000, stopOnInteraction: true, stopOnMouseEnter: true })
+    Autoplay({ delay: 4000, stopOnInteraction: true, stopOnMouseEnter: true })
   );
 
   return (
